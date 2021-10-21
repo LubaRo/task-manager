2208ae3dd414a16c38ad673ed48453de82525161
Lyubov Parfilova <cnlmode@gmail.com>
2021-10-21 21:12:54 +0400

    add state machine for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,38 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
   validates :author, presence: true
+
+  state_machine :state, initial: :new_task do
+    event :develop do
+      transition :new_task => :in_development
+    end
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+
+    event :in_qa do
+      transition :in_development => :in_qa
+    end
+
+    event :make_bug_fixes do
+      transition :in_qa => :in_development
+    end
+
+    event :review_code do
+      transition :in_qa => :in_code_review
+    end
+
+    event :ready_for_release do
+      transition :in_code_review => :ready_for_release
+    end
+
+    event :make_code_fixes do
+      transition :in_code_review => :in_development
+    end
+
+    event :release do
+      transition :ready_for_release => :released
+    end
+  end
 end
