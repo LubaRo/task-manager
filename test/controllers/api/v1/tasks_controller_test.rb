f3258d071da07563a01c286aece0de0b7a6c1c5b
Lyubov Parfilova <cnlmode@gmail.com>
2021-10-27 20:29:53 +0400

    api: add show action

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class Api::V1::TasksControllerTest < ActionController::TestCase
-  # test 'should get show' do
-  #   author = create :author
-  #   task = create :task, author: author
-  #   get :show, params: { id: task.id, format: :json }
-  #   assert_response :success
-  # end
+  test 'should get show' do
+    author = create(:user)
+    task = create(:task, author: author)
+    get :show, params: { id: task.id, format: :json }
+    assert_response :success
+  end
 
   test 'should get index' do
     get :index, params: { format: :json }
