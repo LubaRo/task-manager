f1577f3e51190f60319d31ed796b500890f0bae3
Lyubov Parfilova <cnlmode@gmail.com>
2021-11-10 15:00:59 +0400

    add lcov formatter for simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'simplecov'
+require 'simplecov-lcov'
 
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start 'rails'
 
 class ActiveSupport::TestCase
