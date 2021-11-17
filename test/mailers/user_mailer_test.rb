b82bd839be15950ca26b93e6f51dcc67437883fb
Lyubov Parfilova <cnlmode@gmail.com>
2021-11-17 13:12:47 +0400

    send email on task create, update and delete

---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  setup do
+    @user = create(:user)
+    @task = create(:task, author: @user)
+  end
+
+  test 'task created' do
+    params = { user: @user, task: @task }
+    email = UserMailer.with(params).task_created
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [@user.email], email.to
+    assert_equal 'New Task Created', email.subject
+    assert email.body.to_s.include?("Task #{@task.id} was created")
+  end
+
+  test 'task updated' do
+    params = { user: @user, task: @task }
+    email = UserMailer.with(params).task_updated
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [@user.email], email.to
+    assert_equal 'Task Updated', email.subject
+    assert email.body.to_s.include?("Task #{@task.id} was updated")
+  end
+
+  test 'task deleted' do
+    params = { user: @user, task: @task }
+    email = UserMailer.with(params).task_deleted
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [@user.email], email.to
+    assert_equal 'Task Deleted', email.subject
+    assert email.body.to_s.include?("Task #{@task.id} was deleted")
+  end
+end
