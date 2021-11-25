c2e96d9bdeb2ca26bfab0bf35e98a2f43c510551
Lyubov Parfilova <cnlmode@gmail.com>
2021-11-25 12:48:10 +0400

    setup default_url_options for entire app

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.routes.default_url_options = { host: ENV.fetch('DEFAULT_HOST', 'localhost'), port: ENV.fetch('DEFAULT_PORT', 3000) }
